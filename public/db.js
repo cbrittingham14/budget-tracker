e3139ef7ce12d078bb40265f26300ff3928fc1b4
chizz <cbrittingham@gmail.com>
2020-04-14 17:32:35 -0500

    added comments

--- public/db.js
+++ public/db.js
@@ -2,23 +2,28 @@
 let db; //set blank global let to assign db during request.onsuccess 
 const request = indexedDB.open("budget", 1);
 
+
 request.onupgradeneeded = e => {
 	const db = e.target.result;
 	db.createObjectStore('pending', { autoIncrement: true });
 };
 
+//function to save to indexedDb
+//called when fetch request fails (assuming internet is down)
 function save(data){
 	const trans = db.transaction(['pending'], 'readwrite');
 	const store = trans.objectStore('pending');
 	store.add(data);
 };
 
+//key function to check indexedDb
 function checkDb() {
 
 	const trans = db.transaction(['pending'], 'readwrite');
 	const store = trans.objectStore('pending');
 	const all = store.getAll();
 
+		// if there are any items in the store post them all to the database
 		all.onsuccess = () => {
 			if (all.result.length > 0){
 				fetch('/api/transaction/bulk', {
@@ -40,8 +45,9 @@ function checkDb() {
 			}
 		};
 };
-request.onsuccess = e => {
 
+// called when indexedDb is successfully opened
+request.onsuccess = e => {
 	db = e.target.result; //set db to indexedDb instance
 	if(navigator.onLine){
 		checkDb();
@@ -52,5 +58,5 @@ request.onerror = e => {
   console.log('== ERROR ==> ', e.target.errorCode);
 };
 
-// check indexedDB when window goes online
+// call checkDb when online connection returns
 window.addEventListener('online', checkDb);
